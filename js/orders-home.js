ccb6cd87b11a1c6b48120efeebf7f1e7657f75ca
Pierce Thompson <pqt5055@psu.edu>
2021-03-27 23:32:43 -0400

    Implemented cart modal population

--- js/orders-home.js
+++ js/orders-home.js
@@ -1,34 +1,142 @@
 var cart_modal_btn = document.getElementById("cart_icon");
 var cart_modal = document.getElementById("cart_modal");
 var sticky = cart_modal_btn.offsetTop;
-var span = document.getElementsByClassName('close')[0];
+var modal_close = document.getElementsByClassName('close')[0];
 
-console.log(cart_modal_btn);
-console.log(cart_modal);
-console.log(sticky);
-console.log(span);
+var menu =  {
+    1 : "Buffalo Tots",
+    2 : "Tots in a blanket",
+    3 : "Loaded Tots",
+    4 : "Tot Nachos",
+    5 : "Beer Fried Tots",
+    6 : "True tater salad",
+    7 : "Reverse tater salad",
+    8 : "Grilled chicken tater salad",
+    9 : "Tiny tater salad",
+    10 : "Singular tot",
+    11 : "Chicken Tot Pie",
+    12 : "Pulled Pork Tots",
+    13 : "Steak and Tots",
+    14 : "Tot Burrito",
+    15 : "Tot tacos",
+    16 : "Tater Pizza",
+    17 : "Chicken Fried tots",
+    18 : "Classic Tater burger",
+    19 : "The Big Tater burger",
+    20 : "Tater Roma",
+    21 : "Philly Cheese Tater",
+    22 : "Captain Tot",
+    23 : "The Seven Tots",
+    24 : "Tots",
+    25 : "Tater Chips",
+    26 : "Hash Browns",
+    27 : "Waffle Tots",
+    28 : "Bottled Water",
+    29 : "Soda Water",
+    30 : "Coors Light"
+}
 
+var cart = [];
+var cart_elements = [];
 
+// Calls
 cart_modal_btn.onclick = function () {
+    var order = getOrder(cart);
+    cart_elements = populateCartModal(order);
     cart_modal.style.display = "block";
 }
 
-span.onclick = function () {
+modal_close.onclick = function () {
     cart_modal.style.display = "none";
+    clearCartModal(cart_elements);
 }
 
 window.onclick = function (event) {
     if (event.target === cart_modal) {
         cart_modal.style.display = "none";
+        clearCartModal(cart_elements);
     }
 }
 
 window.onscroll = function() {cartScroll()};
+
+// Functions
 function cartScroll() {
     if(window.pageYOffset > sticky) {
         cart_modal_btn.classList.add("fixed_cart");
     }
     else {
         cart_modal_btn.classList.remove("fixed_cart");
+    }
+}
+
+function addToCart(value) {
+    console.log("Pushing: " + menu[value] + " to cart | Menu Index: " + value);
+    cart.push(value);
+}
+
+function getOrder(cart) {
+    // Make 2 arrays of equal length
+    // list of items
+    // list of frequency of each item
+    var items = [], freq = [], prev = -1;
+
+    cart.sort();
+    for(var i = 0; i < cart.length; i++) {
+        if (cart[i] !== prev) {
+            items.push(cart[i]);
+            freq.push(1);
+        } else {
+            freq[freq.length - 1]++;
+        }
+        prev = cart[i];
+    }
+
+    var order = [];
+    // Create array of "tuples" containing
+    // (item index, frequency of item)
+    for(var j = 0; j < items.length; j++) {
+        order.push([items[j], freq[j]]);
+    }
+    console.log(order);
+    return order;
+}
+
+function populateCartModal(order) {
+    var modal_body = document.getElementsByClassName("modal_body")[0];
+
+    var cart_elements = [];
+    for(var i = 0; i < order.length; i++) {
+        // Create elements for modal body
+        var cart_item_cont = document.createElement('div');
+        var cart_item = document.createElement('p');
+        var cart_item_quant = document.createElement('p');
+
+        // Give item container its class
+        cart_item_cont.setAttribute('class', 'modal_cart_item');
+
+        // Give item container elements their text
+        cart_item.textContent = menu[order[i][0]];  // Menu Item
+        cart_item_quant.textContent = order[i][1];              // Quantity
+
+        // Set Children
+        cart_item_cont.appendChild(cart_item);
+        cart_item_cont.appendChild(cart_item_quant);
+        modal_body.appendChild(cart_item_cont);
+
+        cart_elements.push([cart_item_cont, [cart_item, cart_item_quant]]);
+    }
+
+    return cart_elements;
+}
+
+function clearCartModal(cart_elements) {
+    var modal_body = document.getElementsByClassName("modal_body")[0];
+
+    for(var i = 0; i < cart_elements.length; i++) {
+        cart_elements[i][0].removeChild(cart_elements[i][1][0]);
+        cart_elements[i][0].removeChild(cart_elements[i][1][1]);
+
+        modal_body.removeChild(cart_elements[i][0]);
     }
 }
