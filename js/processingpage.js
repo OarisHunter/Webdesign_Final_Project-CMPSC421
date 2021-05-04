364cc6f3b42b27a34a846075be3961e248f0213a
bkm5322 <bkm5322@psu.edu>
2021-05-04 06:57:00 -0400

    Completed js implementation backend

--- js/processingpage.js
+++ js/processingpage.js
@@ -1,13 +1,23 @@
 function getOrderStatus() {
+  var email = document.getElementById("email").value;
+  if (!email || !email.length){
+    alert('Please enter e-mail to query');
+    return;
+  }
+
   var xmlhttp = new XMLHttpRequest();
-  var url = "http://localhost:3000/get/order/status?orderName=drew";
+  var url = "http://localhost:3000/get/order/status?email=" + encodeURIComponent(email);
 
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
       console.log(this.responseText);
-      let resObj = JSON.parse(this.responseText)
-      document.getElementById("timeLeft").innerText = resObj.timeLeft
-      document.getElementById("orderName").innerText = resObj.orderName
+      let resObj = JSON.parse(this.responseText);
+      if (!resObj || !resObj.order) {
+        alert("No Order Found with that e-mail address");
+        return;
+      }
+      document.getElementById("orderName").innerText = resObj.order
+      document.getElementById("orderStatus").innerText = getOrderStatusString(resObj.orderStatus);
     }
     else {
       console.log('status', xmlhttp.status)
@@ -18,6 +28,15 @@ function getOrderStatus() {
 
 }
 
+function getOrderStatusString(statusNumber) {
+  if (statusNumber == 1) {
+    return "Completed";
+  } else if(statusNumber == 2) {
+    return "In Cooking";
+  }
+  return "Unknown";
+}
+
 window.onload = function () {
-  getOrderStatus()
+  // getOrderStatus()
 }
